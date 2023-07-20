d0c7e4909ad400101592f00bdc525609f491d79a
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-20 04:37:10 +0000

    five

--- src/components/App.js
+++ src/components/App.js
@@ -3,36 +3,40 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [gender, setGender] = useState("male");
+  const [gender, setGender] = useState("shirt");
 
   return (
     <div>
         {/* Do not remove the main div */}
         <div>
           <h1>Select your gender:</h1>
-          <label><input type="radio" onClick={() => setGender("male")}/> Male</label>
-          <label><input type="radio" onClick={() => setGender("female")} />Female</label> 
+          <label><input type="radio" onClick={() => setGender("shirt")}/> Male</label>
+          <label><input type="radio" onClick={() => setGender("dress")} />Female</label> 
+        </div>
+        <div>
+        <h2>Select your {gender} size:</h2>
+
+        {
+          (gender === "shirt") ? 
+
+          (<select name="shirt">
+            <option value="">Select size</option>
+            <option value="small">Small</option>
+            <option value="meduim">Meduim</option>
+            <option value="large">Large</option>
+          </select>)
+          :
+         (<select name="dress">
+            <option value="">Select size</option>
+            <option value="2">2</option>
+            <option value="4">4</option>
+            <option value="6">6</option>
+          </select>)
+        }
+
         </div>
         
-          {
-            (gender === "male") ? <div>
-              <h2>Select your shirt size:</h2>
-              <select name="shirt">
-                <option value="#">Select size</option>
-                <option value="small">Small</option>
-                <option value="medium">Medium</option>
-                <option value="large">Large</option>
-              </select>
-            </div> : <div>
-            <h2>Select your dress size:</h2>
-              <select name="dress">
-                <option value="#">Select size</option>
-                <option value="2">2</option>
-                <option value="4">4</option>
-                <option value="6">6</option>
-              </select>
-            </div>
-          }
+          
         
     </div>
   )
